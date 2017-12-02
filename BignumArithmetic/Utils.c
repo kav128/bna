f1485ac752d2e0bcdfac82795fa948b8706e799c
Артем Крылов <kav128@outlook.com>
2017-12-02 14:43:58 +0300

    Реализованы служебные функции для работы с длинными числами и памятью

---
+++ BignumArithmetic/Utils.c
@@ -0,0 +1,63 @@
+﻿#include <malloc.h>
+#include <string.h>
+#include "Utils.h"
+
+// Размер всех строк для хранения чисел и промежуточных вычислений
+const size_t BufSize = 64;
+
+// Избавляемся от лидирующих нулей
+void ZeroTrim(char *str)
+{
+	size_t shift = 0;
+	// Вдруг число отрицательное
+	int start = *str == '-' ? 1 : 0;
+	size_t len = strlen(str) + 1;
+	while (*(str + start + shift) == '0')
+	{
+		shift++;
+	}
+	if (shift == len - 1)
+	{
+		shift--;
+	}
+
+	memcpy(str + start, str + start + shift, len - shift);
+	len -= shift;
+	// Зануляем байты в конце строки
+	memset(str + start + len, 0, len);
+}
+
+// Добавляем лидирующие нули
+void AddZeros(char *str, size_t zcount)
+{
+	size_t len = strlen(str);
+	memcpy(str + zcount, str, len);
+	memset(str, '0', zcount);
+}
+
+// Проверяем, является ли строка правильным числом
+char Validate(char *str)
+{
+	char res = 1;
+	if (*str == '-')
+	{
+		str++;
+	}
+
+	do
+	{
+		if (*str < '0' || *str > '9')
+		{
+			res = 0;
+		}
+	}
+	while (*(++str) != 0 && res == 1);
+
+	return res;
+}
+
+// Подчищаем всю строку
+void Erase(char *str)
+{
+	memset(str, 0, BufSize);
+}
---
+++ BignumArithmetic/Utils.h
@@ -0,0 +1,9 @@
+﻿#ifndef BIGNUM_UTILS_H
+#define BIGNUM_UTILS_H
+
+void ZeroTrim(char *str);
+void Erase(char *str);
+void AddZeros(char *str, size_t zcount);
+char Validate(char *str);
+
+#endif //BIGNUM_UTILS_H
