1a8bbc770561aac81c4ec2a8435d19402ac80458
Артем Крылов <kav128@outlook.com>
2017-12-30 17:30:51 +0300

    Небольшие изменения в структуре программы и логике ввода
    Fixes #1, fixes #2, fixes #6

--- BignumArithmetic/Source.c
+++ BignumArithmetic/Source.c
@@ -10,59 +10,16 @@ extern size_t BufSize;
 inline void pHeader();
 inline void pHelp(char clrmode);
 inline void pConsoleHelp(char clrmode);
+inline int Calculate(char *a, char *b, char *r, char op);
+inline int ReadParameters(int argc, char* argv[], char *clrmode);
+inline int Input(char *a, char *b, char *op, char clrmode);
 
 int main(int argc, char* argv[])
 {
-	
-	char flg = 1; // Пока все хорошо
 	char clrmode = 0;
-	// Читаем аргументы командной строки
-	for (int i = 1; i < argc; i++)
-	{
-		char correct = 0; // Пока аргумент не подошел ни под один обработчик 
-		// Задание размера буферов для чисел
-		if (!strcmp(argv[i], "-bsize"))
-		{
-			correct = 1;
-			if (argc - 1> i)
-			{
-				if (Validate(argv[i + 1]))
-				{
-					BufSize = atoi(argv[i + 1]);
-					// Перескакиваем через один аргумент - мы его уже учли
-					i++;
-				}
-				else
-				{
-					// Ввели некорректное число
-					printf("Invalid numberic input \"%s\"\n", argv[i + 1]);
-					flg = 0;
-					break;
-				}
-			}
-			else
-			{
-				// Пропустили задание размера после аргумента -bsize
-				printf("Buffer size is missing after \"-bsize\" argument\n");
-				flg = 0;
-				break;
-			}
-		}
-		// "Чистый вывод" - только результаты вычислений или сообщения об ошибках. Справка работает, если запрошена
-		if (!strcmp(argv[i], "-clear"))
-		{
-			correct = 1;
-			clrmode = 1;
-		}
-		// А правильный ли вообще аргумент нам подкинули?
-		if (!correct)
-		{
-			printf("Invalid argument \"%s\"\n", argv[i]);
-			flg = 0;
-		}
-	}
+	int pcorrect = ReadParameters(argc, argv, &clrmode);
 
-	if (!flg)
+	if (!pcorrect)
 	{
 		pHelp(clrmode);
 		return 0;
@@ -80,67 +37,19 @@ int main(int argc, char* argv[])
 
 	while (1)
 	{
-		if (!clrmode)
-		{
-			printf("a> ");
-		}
-		scanf("%s", a);
-		if (!strncmp(a, "exit", 4))
+		char op;
+		int inp = Input(a, b, &op, clrmode);
+		if (inp == 1)
 		{
 			break;
 		}
-		if (!strncmp(a, "help", 4))
+		if (inp == 2)
 		{
 			pHelp(clrmode);
-			Erase(a);
 			continue;
 		}
-		if (!Validate(a))
-		{
-			printf("Invalid number a\n");
-			Erase(a);
-			continue;
-		}
-		if (!clrmode)
-		{
-			printf("b> ");
-		}
-		scanf("%s", b);
-		if (!Validate(b))
-		{
-			printf("Invalid number b\n");
-			Erase(a);
-			Erase(b);
-			continue;
-		}
-		if (!clrmode)
-		{
-			printf("o> ");
-		}
-		char op;
-		getchar(); // Символ конца строки
-		scanf("%c", &op);
-		switch (op)
-		{
-			case '+':
-				Add(a, b, r);
-				break;
-			case '-':
-				Sub(a, b, r);
-				break;
-			case '*':
-				Mul(a, b, r);
-				break;
-			case '/':
-				Div(a, b, r);
-				break;
-			default:
-				printf("Invalid operation\n");
-				Erase(a);
-				Erase(b);
-				continue;
-				break;
-		}
+		
+		Calculate(a, b, r, op);
 
 		if (!clrmode)
 		{
@@ -198,4 +107,145 @@ inline void pConsoleHelp(char clrmode)
 		printf("89\n");
 		printf("+\n\n");
 	}
+}
+
+inline int ReadParameters(int argc, char* argv[], char *clrmode)
+{
+	char paramsCorrect = 1; // Пока все хорошо
+							// Читаем аргументы командной строки
+	for (int i = 1; i < argc; i++)
+	{
+		char argCorrect = 0; // Пока аргумент не подошел ни под один обработчик 
+						  // Задание размера буферов для чисел
+		if (!strcmp(argv[i], "-bsize"))
+		{
+			argCorrect = 1;
+			if (argc - 1> i)
+			{
+				if (Validate(argv[i + 1]))
+				{
+					BufSize = atoi(argv[i + 1]);
+					// Перескакиваем через один аргумент - мы его уже учли
+					i++;
+				}
+				else
+				{
+					// Ввели некорректное число
+					printf("Invalid numberic input \"%s\"\n", argv[i + 1]);
+					paramsCorrect = 0;
+					break;
+				}
+			}
+			else
+			{
+				// Пропустили задание размера после аргумента -bsize
+				printf("Buffer size is missing after \"-bsize\" argument\n");
+				paramsCorrect = 0;
+				break;
+			}
+		}
+		// "Чистый вывод" - только результаты вычислений или сообщения об ошибках. Справка работает, если запрошена
+		if (!strcmp(argv[i], "-clear"))
+		{
+			argCorrect = 1;
+			*clrmode = 1;
+		}
+		// А правильный ли вообще аргумент нам подкинули?
+		if (!argCorrect)
+		{
+			printf("Invalid argument \"%s\"\n", argv[i]);
+			paramsCorrect = 0;
+		}
+	}
+
+	return paramsCorrect;
+}
+
+inline int Calculate(char *a, char *b, char *r, char op)
+{
+	switch (op)
+	{
+		case '+':
+			Add(a, b, r);
+			break;
+		case '-':
+			Sub(a, b, r);
+			break;
+		case '*':
+			Mul(a, b, r);
+			break;
+		case '/':
+			Div(a, b, r);
+			break;
+		default:
+			return 0; // Оператор неверный
+	}
+	return 1; // Все ок
+}
+
+inline int Input(char *a, char *b, char *op, char clrmode)
+{
+	while (1)
+	{
+		if (!clrmode)
+		{
+			printf("a> ");
+		}
+		scanf("%s", a);
+		if (!strncmp(a, "exit", 4))
+		{
+			return 1; // Код возврата, если прилетело "exit"
+		}
+		if (!strncmp(a, "help", 4))
+		{
+			return 2; // Код возврата, если прилетело "help"
+		}
+		if (!Validate(a))
+		{
+			printf("Invalid number a\n");
+			Erase(a);
+		}
+		else
+		{
+			break;
+		}
+	}
+
+	while (1)
+	{
+		if (!clrmode)
+		{
+			printf("b> ");
+		}
+		scanf("%s", b);
+		if (!Validate(b))
+		{
+			printf("Invalid number b\n");
+			Erase(b);
+		}
+		else
+		{
+			break;
+		}
+	}
+
+	while (1)
+	{
+		if (!clrmode)
+		{
+			printf("o> ");
+		}
+		getchar(); // Символ конца строки
+		scanf("%c", op);
+
+		if (*op == '+' || *op == '-' || *op == '*' || *op == '/')
+		{
+			break;
+		}
+		else
+		{
+			printf("Invalid operator\n");
+		}
+	}
+	return 0; // Введено все. Можно выполнять вычисления
 }
